ec06f6a5ba8e730c540d2b3b3254957897a9b995
Andrey Arekhva <a.arekhva@gmail.com>
2025-02-17 13:32:24 +0300

    Copy the source code of Plugin.Maui.CalendarStore package and fix bugs (#3)
    * Copy the source code of Plugin.Maui.CalendarStore package
    
    * Get recurring events on Android
    
    * Handle timezone exceptions

--- ToDoCalendar/ToDoCalendar.MauiHybrid/MauiProgram.cs
+++ ToDoCalendar/ToDoCalendar.MauiHybrid/MauiProgram.cs
@@ -31,11 +31,7 @@ public static class MauiProgram
         builder.Logging.AddDebug();
 #endif
 
-#if ANDROID
-        builder.Services.AddSingleton<ICalendarService, Platforms.Android.CalendarService>();
-#else
         builder.Services.AddSingleton<ICalendarService, CalendarService>();
-#endif
 
         builder.Services.AddSingleton<IDispatcher>(provider =>
         {
