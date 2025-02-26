262ae4d81e20d94e3c96389d3ec0fb46f41ab6c6
Andrey Arekhva <a.arekhva@gmail.com>
2025-02-26 16:31:37 +0300

    Hide editing popup when the virtual keyboard is closed - #18 (#49)

---
+++ ToDoCalendar/ToDoCalendar.MauiHybrid/Platforms/Android/KeyboardService.cs
@@ -0,0 +1,44 @@
+﻿using ToDoCalendarControl.Services;
+
+namespace ToDoCalendar.MauiHybrid.Platforms.Android;
+
+public class KeyboardService : IKeyboardService
+{
+    public event Action<bool>? KeyboardStateChanged;
+
+    public KeyboardService()
+    {
+        var rootView = Platform.CurrentActivity?.Window?.DecorView.FindViewById(global::Android.Resource.Id.Content);
+
+        if (rootView != null)
+        {
+            var listener = new KeyboardListener(() => rootView.Height, isOpen => KeyboardStateChanged?.Invoke(isOpen));
+            rootView.ViewTreeObserver?.AddOnGlobalLayoutListener(listener);
+        }
+    }
+
+    private class KeyboardListener : Java.Lang.Object, global::Android.Views.ViewTreeObserver.IOnGlobalLayoutListener
+    {
+        private readonly Func<int> _getViewHeight;
+        private readonly Action<bool> _stateChanged;
+
+        private int _previousHeight;
+
+        public KeyboardListener(Func<int> getViewHeight, Action<bool> stateChanged)
+        {
+            _getViewHeight = getViewHeight;
+            _stateChanged = stateChanged;
+            _previousHeight = _getViewHeight();
+        }
+
+        public void OnGlobalLayout()
+        {
+            var height = _getViewHeight();
+            int heightDiff = _previousHeight - height;
+            bool isKeyboardOpen = heightDiff > 200; // Threshold to detect keyboard
+
+            _stateChanged(isKeyboardOpen);
+            _previousHeight = height;
+        }
+    }
+}
---
+++ ToDoCalendar/ToDoCalendar.MauiHybrid/Platforms/iOS/KeyboardService.cs
@@ -0,0 +1,28 @@
+﻿using Foundation;
+using ToDoCalendarControl.Services;
+using UIKit;
+
+namespace ToDoCalendar.MauiHybrid.Platforms.iOS;
+
+public class KeyboardService : IKeyboardService
+{
+    public event Action<bool>? KeyboardStateChanged;
+
+    public KeyboardService()
+    {
+        var notificationCenter = NSNotificationCenter.DefaultCenter;
+
+        notificationCenter.AddObserver(UIKeyboard.DidShowNotification, OnKeyboardWillShow);
+        notificationCenter.AddObserver(UIKeyboard.DidHideNotification, OnKeyboardWillHide);
+    }
+
+    private void OnKeyboardWillShow(NSNotification notification)
+    {
+        KeyboardStateChanged?.Invoke(true);
+    }
+
+    private void OnKeyboardWillHide(NSNotification notification)
+    {
+        KeyboardStateChanged?.Invoke(false);
+    }
+}
