86b90302f21703fd8489e03e61f95056d646f65f
Andrei Arekhva <a.arekhva@gmail.com>
2025-02-28 13:46:08 +0300

    Prevent text selection - fixes #43

--- ToDoCalendar/ToDoCalendar/App.xaml.cs
+++ ToDoCalendar/ToDoCalendar/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using OpenSilver;
+using System.Windows;
 using ToDoCalendar.Utils;
 
 #if !OPENSILVER
@@ -20,8 +21,12 @@ namespace ToDoCalendar
         private async void App_Startup(object sender, StartupEventArgs e)
         {
             await FontLoader.LoadAppFonts();
+
             var mainPage = new MainPage();
             Window.Current.Content = mainPage;
+
+            // prevent text selection
+            Interop.ExecuteJavaScriptVoidAsync("$0.classList.add('opensilver-pointer-captured')", Interop.GetDiv(mainPage));
         }
 
         /*
